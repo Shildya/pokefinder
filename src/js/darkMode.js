825ab2df4dc1ecfa9d544ca2aac60b6c60ffee2a
Shildya <nicolas@jesenberger.fr>
2020-05-06 10:50:51 +0200

    infos.html fix

---
+++ src/js/darkMode.js
@@ -0,0 +1,61 @@
+var moon = document.querySelector('.moon');
+var sun = document.querySelector('.sun');
+const body = document.querySelector('body');
+const logoDark = document.querySelector('.logo-dark');
+const logoLight = document.querySelector('.logo-light');
+const header = document.querySelector('header');
+const searchBar = document.querySelector('.search-bar')
+const footer = document.querySelector('footer');
+const noResult = document.querySelector('.no-result');
+
+function darkMode() {
+
+    var pokemons = document.querySelectorAll('.pokemon');
+    pokemons.forEach(pokemon => {
+        pokemon.classList.toggle('dark-mode')
+    });
+
+    var pokemonIds = document.querySelectorAll('.pokemon-id');
+    pokemonIds.forEach(pokemonId => {
+        pokemonId.classList.toggle('dark-mode');
+    });
+
+    var pokemonNames = document.querySelectorAll('.pokemon-name');
+    pokemonNames.forEach(pokemonName => {
+        pokemonName.classList.toggle('dark-mode');
+    });
+
+    header.classList.toggle('dark-mode');
+    body.classList.toggle('dark-mode');
+    footer.classList.toggle('dark-mode')
+
+    logoLight.classList.toggle('hidden');
+    logoDark.classList.toggle('hidden');
+
+    moon.classList.toggle('hidden');
+    sun.classList.toggle('hidden');
+
+    searchBar.classList.toggle('dark-mode');
+    noResult.classList.toggle('dark-mode');
+};
+
+moon.addEventListener('click', darkMode);
+sun.addEventListener('click', darkMode);
+
+searchBar.addEventListener('input', () => {
+    document.querySelectorAll('.pokemon').forEach(pokemon => {
+        var pokemonName = pokemon.querySelector('.pokemon-name').textContent;
+        if (!pokemonName.startsWith(searchBar.value.toLowerCase())) {
+            pokemon.classList.add('hidden');
+        }
+        else {
+            pokemon.classList.remove('hidden');
+        }
+    });
+    if (document.querySelectorAll('.pokemon.hidden').length === limit) {
+        document.querySelector('.no-result').classList.remove('hidden');
+    }
+    else {
+        document.querySelector('.no-result').classList.add('hidden');
+    }
+});
--- src/js/javascript.js
+++ src/js/javascript.js
@@ -18,7 +18,7 @@ for (i = 1; i < limit + 1; i++) {
 
     var pokemonName = document.createElement('a');
     pokemonName.setAttribute('class', 'pokemon-name');
-    pokemonName.href = 'infos.html';
+    pokemonName.href = '/infos.html';
     pokemon.appendChild(pokemonName);
 
     var pokemonTypes = document.createElement('p');
@@ -55,66 +55,3 @@ for (i = 1; i < limit + 1; i++) {
         allPokemons.push(`${data.name}`);
     });
 };
-
-
-var moon = document.querySelector('.moon');
-var sun = document.querySelector('.sun');
-const body = document.querySelector('body');
-const logoDark = document.querySelector('.logo-dark');
-const logoLight = document.querySelector('.logo-light');
-const header = document.querySelector('header');
-const searchBar = document.querySelector('.search-bar')
-const footer = document.querySelector('footer');
-const noResult = document.querySelector('.no-result');
-
-function darkMode() {
-
-    var pokemons = document.querySelectorAll('.pokemon');
-    pokemons.forEach(pokemon => {
-        pokemon.classList.toggle('dark-mode')
-    });
-
-    var pokemonIds = document.querySelectorAll('.pokemon-id');
-    pokemonIds.forEach(pokemonId => {
-        pokemonId.classList.toggle('dark-mode');
-    });
-
-    var pokemonNames = document.querySelectorAll('.pokemon-name');
-    pokemonNames.forEach(pokemonName => {
-        pokemonName.classList.toggle('dark-mode');
-    });
-
-    header.classList.toggle('dark-mode');
-    body.classList.toggle('dark-mode');
-    footer.classList.toggle('dark-mode')
-
-    logoLight.classList.toggle('hidden');
-    logoDark.classList.toggle('hidden');
-
-    moon.classList.toggle('hidden');
-    sun.classList.toggle('hidden');
-
-    searchBar.classList.toggle('dark-mode');
-    noResult.classList.toggle('dark-mode');
-};
-
-moon.addEventListener('click', darkMode);
-sun.addEventListener('click', darkMode);
-
-searchBar.addEventListener('input', () => {
-    document.querySelectorAll('.pokemon').forEach(pokemon => {
-        var pokemonName = pokemon.querySelector('.pokemon-name').textContent;
-        if (!pokemonName.startsWith(searchBar.value.toLowerCase())) {
-            pokemon.classList.add('hidden');
-        }
-        else {
-            pokemon.classList.remove('hidden');
-        }
-    });
-    if (document.querySelectorAll('.pokemon.hidden').length === limit) {
-        document.querySelector('.no-result').classList.remove('hidden');
-    }
-    else {
-        document.querySelector('.no-result').classList.add('hidden');
-    }
-});
